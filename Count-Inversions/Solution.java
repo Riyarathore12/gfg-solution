e376670db76addf4ebd2859c04f0b8ebdc63b503
Riya Rathore <riyarathore1825@gmail.com>
2026-06-12 17:28:29 +0530

    Add Solution for Count Inversions [CodeSync]

--- Count-Inversions/Solution.java
+++ Count-Inversions/Solution.java
@@ -1,15 +1,3 @@
-class Solution {
-
-    static long inversionCount(int arr[]) {
-        return mergeSort(arr, 0, arr.length - 1);
-    }
-
-    static long mergeSort(int[] arr, int left, int right) {
-        long count = 0;
-
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-
             count += mergeSort(arr, left, mid);
             count += mergeSort(arr, mid + 1, right);
             count += merge(arr, left, mid, right);
@@ -19,3 +7,15 @@ class Solution {
     }
 
     static long merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+        long inv = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
