7b55d598ce0f3549cb97bf7af19bcdf65ffcf12e
Riya Rathore <riyarathore1825@gmail.com>
2026-06-17 10:32:18 +0530

    Add Solution for Cut rope to maximise product [CodeSync]

--- Cut-rope-to-maximise-product/Solution.java
+++ Cut-rope-to-maximise-product/Solution.java
@@ -1,13 +1,23 @@
 class Solution {
     public int maxProduct(int n) {
         // code here
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        for(int i = 2; i<=n; i++){
-            for(int j = 1; j<i; j++){
-                dp[i] = Math.max(dp[i] , Math.max(j*(i-j),j*dp[i-j]));
-            }
+        // int[] dp = new int[n+1];
+        // dp[1] = 1;
+        // for(int i = 2; i<=n; i++){
+        //     for(int j = 1; j<i; j++){
+        //         dp[i] = Math.max(dp[i] , Math.max(j*(i-j),j*dp[i-j]));
+        //     }
+        // }
+        // return dp[n];
+        if(n==2){
+            return 1;
         }
-        return dp[n];
+        if(n==3) return 2;
+        int product = 1;
+        while(n>4){
+            product = product*3;
+            n = n-3;
+        }
+        return product*n;
     }
 }
