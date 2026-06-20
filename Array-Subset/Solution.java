9f04cf5831faeec62784cc6d4b62f706dec72680
Riya Rathore <riyarathore1825@gmail.com>
2026-06-20 19:37:58 +0530

    Add Solution for Array Subset [CodeSync]

---
+++ Array-Subset/Solution.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public boolean isSubset(int a[], int b[]) {
+        // Your code here
+        Arrays.sort(a);
+        Arrays.sort(b);
+        
+        int n = a.length;
+        int m = b.length;
+        int  i = 0;
+        int j = 0;
+        
+        while(i<n && j<m){
+            if(a[i] == b[j]){
+                i++;
+                j++;
+            }
+            else if(a[i]<b[j]){
+                i++;
+            }else{
+                return false;
+            }
+        }
+        return j == m;
+    }
+}
