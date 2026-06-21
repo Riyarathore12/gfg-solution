d4b3739d0f4c206df6549795dea94948596b83ca
Riya Rathore <riyarathore1825@gmail.com>
2026-06-21 09:24:00 +0530

    Add Solution for Remove character [CodeSync]

---
+++ Remove-character/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    static String removeChars(String str1, String str2) {
+        // code here
+        HashSet<Character> set = new HashSet<>();
+        for(char ch : str2.toCharArray()){
+            set.add(ch);
+        }
+        StringBuilder sb =new StringBuilder();
+        for(char ch : str1.toCharArray()){
+            if(!set.contains(ch)){
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
