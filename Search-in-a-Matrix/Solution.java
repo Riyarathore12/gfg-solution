b7bce67355704515acc92ff755469b7d46bff06d
Riya Rathore <riyarathore1825@gmail.com>
2026-06-22 14:32:26 +0530

    Add Solution for Search in a Matrix [CodeSync]

---
+++ Search-in-a-Matrix/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        
+        int n = mat.length;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<mat[0].length; j++){
+                if(mat[i][j] == x){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
