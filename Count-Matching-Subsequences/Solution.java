181d3d9c13588accfb47bff809acdee8a656ac25
Riya Rathore <riyarathore1825@gmail.com>
2026-06-26 08:40:41 +0530

    Add Solution for Count Matching Subsequences [CodeSync]

---
+++ Count-Matching-Subsequences/Solution.java
@@ -0,0 +1,26 @@
+    //             if(s1.charAt(i) == s2.charAt(j)){
+    //                 list.add(i);
+    //                 list.add(j);
+    //             count = list.size()/n2;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    int mod = 1000000007;
+    int[][] dp = new int[n+1][m+1];
+    
+    for(int i = 0; i<=n; i++){
+        dp[i][0] = 1;
+    }
+    for(int i = 1; i<=n; i++){
+        for(int j  =1; j<=m; j++){
+            if(s1.charAt(i-1) == s2.charAt(j-1)){
+                dp[i][j] = (dp[i-1][j-1]+dp[i-1][j])%mod;
+            }else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    
+    
+    return dp[n][m];
