55e337360cf7804a7d3ec8e0400941be16f2a7f9
Riya Rathore <riyarathore1825@gmail.com>
2026-06-26 09:13:34 +0530

    Add Solution for Maximum Product Subarray [CodeSync]

--- Maximum-Product-Subarray/Solution.java
+++ Maximum-Product-Subarray/Solution.java
@@ -1,19 +1,6 @@
 class Solution {
     int maxProduct(int[] arr) {
         // code here
-        int maxpro = arr[0];
-        int minpro = arr[0];
-        int ans = arr[0];
-        for(int i =1; i<arr.length; i++){
-            if(arr[i]<0){
-                int temp = maxpro;
-                maxpro = minpro;
-                minpro = temp;
-            }
-            maxpro = Math.max(arr[i] , maxpro*arr[i]);
-            minpro = Math.min(arr[i] , minpro*arr[i]);
-            ans = Math.max(ans , maxpro);
-        }
-        return ans;
+       int ma
     }
 }
