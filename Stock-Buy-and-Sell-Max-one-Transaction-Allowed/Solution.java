5db22a8d4db2bbe638515e2f1950673b1ec0a288
Riya Rathore <riyarathore1825@gmail.com>
2026-06-26 09:28:04 +0530

    Add Solution for Stock Buy and Sell – Max one Transaction Allowed [CodeSync]

--- Stock-Buy-and-Sell-Max-one-Transaction-Allowed/Solution.java
+++ Stock-Buy-and-Sell-Max-one-Transaction-Allowed/Solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
         // Code here
-        int n   =prices.length;
-        int minprice  = prices[0];
-        int maxprofit = 0;
-        for(int i = 1; i<n; i++){
-            minprice = Math.min(minprice , prices[i]);
-            maxprofit = Math.max(maxprofit , prices[i] - minprice);
-        }
-        return maxprofit;
+       int n = prices.length;
+       int minprice =prices[0];
+       int profit = 0;
+       for(int i = 0; i<n; i++){
+           minprice = Math.min(minprice , prices[i]);
+           profit = Math.max(profit , prices[i] - minprice);
+       }
+       return profit;
     }
 }
