5873e0b2cf6b4afb1e790fb731b5cb7b61658c96
Riya Rathore <riyarathore1825@gmail.com>
2026-06-27 11:18:48 +0530

    Add Solution for Ways to Tile the Floor [CodeSync]

---
+++ Ways-to-Tile-the-Floor/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int mod = 1000000007;
+    public int countWays(int n, int m) {
+        // code here
+        long[] dp = new long[n+1];
+        for(int i =1; i<=n; i++){
+            if(i<m){
+                dp[i] = 1;
+            }else if(i==m){
+                dp[i] = 2;
+            }else{
+                dp[i] = (dp[i-1]+ dp[i-m])%mod;
+            }
+        }
+        return (int) dp[n];
+        
+    }
+}
