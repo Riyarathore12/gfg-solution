d977133ee8f31e57f47f60deb135cb9765bec5fb
Riya Rathore <riyarathore1825@gmail.com>
2026-06-27 13:08:45 +0530

    Add Solution for Trapping Rain Water [CodeSync]

---
+++ Trapping-Rain-Water/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxWater(int arr[]) {
+        // code here
+        int n = arr.length;
+        int[] leftmax = new int[n];
+        int[] rightmax = new int[n];
+        leftmax[0] = arr[0];
+        for(int i = 1; i<n; i++){
+            leftmax[i] = Math.max(leftmax[i-1] , arr[i]);
+        }
+        rightmax[n-1] = arr[n-1];
+        for(int i = n-2; i>=0; i--){
+            rightmax[i] = Math.max(rightmax[i+1] , arr[i]);
+        }
+        int water = 0;
+        for(int i = 0; i<n; i++){
+            water += Math.min(leftmax[i] , rightmax[i])-arr[i];
+        }
+        return water;
+    }
+}
