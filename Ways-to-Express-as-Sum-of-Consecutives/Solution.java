b64008a3fe959b988f82a4d83526b05031a7d39e
Riya Rathore <riyarathore1825@gmail.com>
2026-07-10 09:49:44 +0530

    Add Solution for Ways to Express as Sum of Consecutives [CodeSync]

---
+++ Ways-to-Express-as-Sum-of-Consecutives/Solution.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int getCount(int n) {
+        int count = 0;
+
+        for (int k = 2; k * (k + 1) / 2 <= n; k++) {
+
+            int numerator = n - (k * (k - 1)) / 2;
+
+            if (numerator > 0 && numerator % k == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
