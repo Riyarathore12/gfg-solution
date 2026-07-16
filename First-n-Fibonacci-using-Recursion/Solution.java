0fb145de682be7597afda3cd0a2c0914b240f036
Riya Rathore <riyarathore1825@gmail.com>
2026-07-16 13:19:05 +0530

    Add Solution for First n Fibonacci using Recursion [CodeSync]

---
+++ First-n-Fibonacci-using-Recursion/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    // Function to return list containing first n fibonacci numbers.
+    public static int[] fibonacciNumbers(int n) {
+        // Your code here
+        int[] arr = new int[n];
+        if(n>=1){
+            arr[0] =0;
+        }
+        if(n>=2){
+            arr[1]=1;
+        }
+        
+     
+        for(int i = 2; i<n; i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        return arr;
+        }
+}
