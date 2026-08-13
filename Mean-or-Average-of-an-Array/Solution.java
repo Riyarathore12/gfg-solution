0aad38ed4a607b882d463b09fdad61c5a732d3ce
Riya Rathore <riyarathore1825@gmail.com>
2026-08-13 21:31:44 +0530

    Add Solution for Mean or Average of an Array [CodeSync]

--- Mean-or-Average-of-an-Array/Solution.java
+++ Mean-or-Average-of-an-Array/Solution.java
@@ -1,14 +1,6 @@
 class Solution {
     public static int findMean(int[] arr) {
         // code here
-        int n = arr.length;
-        int sum = 0;
-        for(int i = 0; i<n; i++ ){
-            sum += arr[i];
-           
-            
-        }
-         int mean = sum / n;
-        return mean;
+        
     }
-}
+};
